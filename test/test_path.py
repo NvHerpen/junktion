30671480a9bc21722b3147457e17d7eeeb576cf5
Noud van Herpen <noud.van.herpen@vdletg.com>
2021-11-09 12:17:22 +0100

    Implemented determinism from X_a

--- test/test_path.py
+++ test/test_path.py
@@ -1,7 +1,7 @@
 import unittest
 from math import cos, pi, sin
 
-from src.path import Path, RightAngleException
+from src.path import Path
 from src.position import Position
 
 from test.commons import assert_path_equal, assert_position_equal
@@ -88,93 +88,130 @@ class TestInterpolate(unittest.TestCase):
     
 
 class TestGenerateCorner(unittest.TestCase):
-    DEFAULT_DELTA_THETA = 0.14279
-    DEFAULT_L = 0.15406
+    SPEED = 0.3
+    N_POINTS = 7
+    L_SEGMENT = 0.253960
+    DELTA_THETA = 0.2243994
 
-    def test_no_corner(self):
-        X_0 = Position(0, 0, 0)
-        X_1 = Position(1, 1, 0.5 * pi)
-
-        with self.assertRaises(RightAngleException):
-            _ = Path.generate_corner(X_0, X_1, n_segments=0)
-    
     def test_1_segment(self):
         X_0 = Position(0, 0, 0)
         X_1 = Position(1, 1, 0.5 * pi)
+        speed = 2   # Make speed so high, that 1 frame already encompasses total corner length
 
-        corner = Path.generate_corner(X_0, X_1, n_segments=1)
+        corner = Path.generate_corner(X_0, X_1, speed)
         exp_p0 = Position(0, 0, 0.25 * pi)
         exp_p1 = Position(1, 1, 0.5 * pi)
 
         assert_path_equal(corner, [exp_p0, exp_p1])
 
-    def test_2_segments(self):
-        X_0 = Position(0, 0, 0)
-        X_1 = Position(1, 1, 0.5 * pi)
-        n_segments = 2
-
-        corner = Path.generate_corner(X_0, X_1, n_segments=n_segments)
-
-        exp_theta0 = (0.5 * pi) * (1 / 3)
-        exp_theta1 = (0.5 * pi) * (2 / 3)
-        exp_L = 1/(cos(exp_theta0) + cos(exp_theta1))
-        exp_p0 = Position(0, 0, exp_theta0)
-        exp_p1 = Position(exp_L * cos(exp_theta0), exp_L * sin(exp_theta0), exp_theta1)
-        exp_p2 = Position(1, 1, 0.5 * pi)
-
-        assert_path_equal(corner, [exp_p0, exp_p1, exp_p2])
-    
-    def test_10_segments(self):
+    def test_low_speed(self):
         X_0 = Position(0, 0, 0)
         X_1 = Position(1, 1, 0.5 * pi)
 
-        corner = Path.generate_corner(X_0, X_1)
+        corner = Path.generate_corner(X_0, X_1, self.SPEED)
 
         exp_p_1 = Position(
-            self.DEFAULT_L * cos(self.DEFAULT_DELTA_THETA), 
-            self.DEFAULT_L * sin(self.DEFAULT_DELTA_THETA), 
-            2 * self.DEFAULT_DELTA_THETA
+            self.L_SEGMENT * cos(self.DELTA_THETA),
+            self.L_SEGMENT * sin(self.DELTA_THETA),
+            2 * self.DELTA_THETA
         )
-        exp_p_m1 = Position(
-            1 - self.DEFAULT_L * sin(self.DEFAULT_DELTA_THETA),
-            1 - self.DEFAULT_L * cos(self.DEFAULT_DELTA_THETA),
-            0.5 * pi - self.DEFAULT_DELTA_THETA
+        exp_p_m2 = Position(
+            1 - self.L_SEGMENT * sin(self.DELTA_THETA),
+            1 - self.L_SEGMENT * cos(self.DELTA_THETA),
+            0.5 * pi - self.DELTA_THETA
         )
 
+        self.assertEqual(len(corner), self.N_POINTS)
         assert_position_equal(corner[1], exp_p_1, places=3)
-        assert_position_equal(corner[-2], exp_p_m1, places=3)
+        assert_position_equal(corner[-2], exp_p_m2, places=3)
+        assert_position_equal(corner[-1], X_1, places=3)
     
     def test_left_down_corner(self):
         X_0 = Position(1, 1, pi)
         X_1 = Position(0, 0, 1.5 * pi)
 
-        corner = Path.generate_corner(X_0, X_1)
+        corner = Path.generate_corner(X_0, X_1, self.SPEED)
 
         exp_p_1 = Position(
-            1 - self.DEFAULT_L * cos(self.DEFAULT_DELTA_THETA), 
-            1 - self.DEFAULT_L * sin(self.DEFAULT_DELTA_THETA), 
-            pi + 2 * self.DEFAULT_DELTA_THETA
+            1 - self.L_SEGMENT * cos(self.DELTA_THETA), 
+            1 - self.L_SEGMENT * sin(self.DELTA_THETA), 
+            pi + 2 * self.DELTA_THETA
         )
         exp_p_m1 = Position(
-            self.DEFAULT_L * sin(self.DEFAULT_DELTA_THETA),
-            self.DEFAULT_L * cos(self.DEFAULT_DELTA_THETA),
-            1.5 * pi - self.DEFAULT_DELTA_THETA
+            self.L_SEGMENT * sin(self.DELTA_THETA),
+            self.L_SEGMENT * cos(self.DELTA_THETA),
+            1.5 * pi - self.DELTA_THETA
         )
 
         assert_position_equal(corner[1], exp_p_1, places=3)
         assert_position_equal(corner[-2], exp_p_m1, places=3)
-    
 
-class TestPath(unittest.TestCase):
+
+class TestCalculateAB(unittest.TestCase):
     def test_straight_path(self):
         X_0 = Position(0, 0, 0)
         X_1 = Position(10, 0, 0)
 
-        path = Path.generate_path(X_0, X_1)
+        AB = Path.calculate_AB(X_0, X_1)
 
-        exp_path = [Position(i, 0, 0) for i in range(11)]
+        self.assertEqual(len(AB), 1)
+        assert_position_equal(AB[0], X_1)
+        
+    def test_corner_right_up(self):
+        X_0 = Position(2, 0, 0)
+        X_1 = Position(8, 8, 0.5 * pi)
 
-        assert_path_equal(path, exp_path)
+        AB = Path.calculate_AB(X_0, X_1)
+
+        exp_x_a = Position(5, 0, 0)
+        exp_x_b = Position(8, 3, 0.5 * pi)
+
+        self.assertEqual(len(AB), 2)
+        assert_position_equal(AB[0], exp_x_a)
+        assert_position_equal(AB[1], exp_x_b)
+    
+    def test_down_left(self):
+        X_0 = Position(4, 5, 1.5 * pi)
+        X_1 = Position(-2, -1, pi)
+
+        AB = Path.calculate_AB(X_0, X_1)
+
+        exp_x_a = Position(4, 2, 1.5 * pi)
+        exp_x_b = Position(1, -1, pi)
+
+        self.assertEqual(len(AB), 2)
+        assert_position_equal(AB[0], exp_x_a)
+        assert_position_equal(AB[1], exp_x_b)
+
+
+class TestPreCalculatePath(unittest.TestCase):
+    def test_straight_path(self):
+        X_0 = Position(0, 0, 0)
+        X_1 = Position(10, 0, 0)
+
+        path = Path.pre_calculate_path(X_0, X_1, 1)
+
+        assert_path_equal(path, [X_1])
+    
+    def test_corner_right_up(self):
+        X_0 = Position(0, 0, 0)
+        X_1 = Position(10, 10, 0.5 * pi)
+        speed = 1
+
+        pre_calculated_path = Path.pre_calculate_path(X_0, X_1, speed)
+
+        exp_n_positions = 13
+        exp_x_a = Position(10 - Path.CORNER_RADIUS, 0, 0.2617)
+        exp_x_b = Position(10, Path.CORNER_RADIUS, 0.5 * pi)
+        exp_x_b_1 = Position(10, Path.CORNER_RADIUS + 1, 0.5 * pi) 
+        exp_x_b_m2 = Position(10, 9, 0.5 * pi)
+
+        self.assertEqual(len(pre_calculated_path), exp_n_positions)
+        assert_position_equal(pre_calculated_path[0], exp_x_a, places=3)
+        assert_position_equal(pre_calculated_path[5], exp_x_b)
+        assert_position_equal(pre_calculated_path[6], exp_x_b_1)
+        assert_position_equal(pre_calculated_path[-2], exp_x_b_m2)
+
 
 
 if __name__ == "__main__":
